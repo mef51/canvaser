6bdb9aa92ee12f4a3dbcea6d4e088e2467e76dba
Mohammed Afif <hms.mohammed@gmail.com>
2012-08-16 11:31:11 -0400

    Changed helper functions to use the Canvas object.

--- canvaser.js
+++ canvaser.js
@@ -30,32 +30,35 @@ function setCanvasSize(width, height, Canvas) {
     Canvas.height = Canvas.canvas.height();
 }
 
-// draw a line from (xf, yf) to (xt, yt)
-function drawLine(xf, yf, xt, yt, context) {
-    context.beginPath();
-    context.moveTo(xf, yf);
-    context.lineTo(xt, yt);
-    context.stroke();
+/**
+* Draw a line from (xf, yf) to (xt, yt)
+* Uses the context from Canvas.context
+*/
+function drawLine(xf, yf, xt, yt, Canvas) {
+    Canvas.context.beginPath();
+    Canvas.context.moveTo(xf, yf);
+    Canvas.context.lineTo(xt, yt);
+    Canvas.context.stroke();
 }
 
-function drawArc(xOrigin, yOrigin, radius, sAngle, eAngle, isAntiClockwise, context) {
-    context.beginPath();
-    context.arc(xOrigin, yOrigin, radius, sAngle, eAngle, isAntiClockwise);
-    context.stroke();
+function drawArc(xOrigin, yOrigin, radius, sAngle, eAngle, isAntiClockwise, Canvas) {
+    Canvas.context.beginPath();
+    Canvas.context.arc(xOrigin, yOrigin, radius, sAngle, eAngle, isAntiClockwise);
+    Canvas.context.stroke();
 }
 
-function drawQuadratic(FromPoint, ToPoint, AnchorPoint, context) {
-    context.beginPath();
-    context.moveTo(FromPoint.x, FromPoint.y);
-    context.quadraticCurveTo(AnchorPoint.x, AnchorPoint.y, ToPoint.x, ToPoint.y);
-    context.stroke();
+function drawQuadratic(FromPoint, ToPoint, AnchorPoint, Canvas) {
+    Canvas.context.beginPath();
+    Canvas.context.moveTo(FromPoint.x, FromPoint.y);
+    Canvas.context.quadraticCurveTo(AnchorPoint.x, AnchorPoint.y, ToPoint.x, ToPoint.y);
+    Canvas.context.stroke();
 }
 
-function drawBezierCurve(FromPoint, ToPoint, AnchorPoint1, AnchorPoint2, context) {
-    context.beginPath();
-    context.moveTo(FromPoint.x, FromPoint.y);
-    context.bezierCurveTo(AnchorPoint1.x, AnchorPoint1.y, AnchorPoint2.x, AnchorPoint2.y, ToPoint.x, ToPoint.y);
-    context.stroke();
+function drawBezierCurve(FromPoint, ToPoint, AnchorPoint1, AnchorPoint2, Canvas) {
+    Canvas.context.beginPath();
+    Canvas.context.moveTo(FromPoint.x, FromPoint.y);
+    Canvas.context.bezierCurveTo(AnchorPoint1.x, AnchorPoint1.y, AnchorPoint2.x, AnchorPoint2.y, ToPoint.x, ToPoint.y);
+    Canvas.context.stroke();
 }
 
 /**
@@ -63,34 +66,34 @@ function drawBezierCurve(FromPoint, ToPoint, AnchorPoint1, AnchorPoint2, context
 * color is a string
 * width is a number
 * height is a number
-* context is the canvas rendering context
+* Canvas is the canvas you want to draw on, it's context will be used.
 */
-function drawBackground(color, width, height, context) {
+function drawBackground(color, width, height, Canvas) {
     // the background and border of the canvas.
-    drawCell(color, "black", {x:0, y:0}, width, height, context);
+    drawCell(color, "black", {x:0, y:0}, width, height, Canvas.context);
 }
 
 /**
 * Draws a cell at cell.x and cell.y of color 'color' and strokes it with 'strokeColor'
-* and of width 'cellWidth' and height 'cellHeight' in the rendering context 'context'. whew.
+* and of width 'cellWidth' and height 'cellHeight' in the rendering context of 'Canvas.context'. whew.
 */
-function drawCell(color, strokeColor, cell, cellWidth, cellHeight, context) {
-    context.fillStyle = color;
-    context.fillRect(cell.x * cellWidth, cell.y * cellHeight, cellWidth, cellHeight);
-    context.strokeStyle = strokeColor;
-    context.strokeRect(cell.x * cellWidth, cell.y * cellHeight, cellWidth, cellHeight);
+function drawCell(color, strokeColor, cell, cellWidth, cellHeight, Canvas) {
+    Canvas.context.fillStyle = color;
+    Canvas.context.fillRect(cell.x * cellWidth, cell.y * cellHeight, cellWidth, cellHeight);
+    Canvas.context.strokeStyle = strokeColor;
+    Canvas.context.strokeRect(cell.x * cellWidth, cell.y * cellHeight, cellWidth, cellHeight);
 }
 
-function setStyle(options, context) {
+function setStyle(options, Canvas) {
     if(typeof options.lineWidth == "number")
-        context.lineWidth = options.lineWidth;
+        Canvas.context.lineWidth = options.lineWidth;
     if(typeof options.strokeStyle == "string")
-        context.strokeStyle = options.strokeStyle;
+        Canvas.context.strokeStyle = options.strokeStyle;
     if(typeof options.lineCap == "string")
-        context.lineCap = options.lineCap;
+        Canvas.context.lineCap = options.lineCap;
     if(typeof options.lineJoin == "string")
-        context.lineJoin = options.lineJoin;
+        Canvas.context.lineJoin = options.lineJoin;
     if(typeof options.fillStyle == "string")
-        context.fillStyle = options.fillStyle;
+        Canvas.context.fillStyle = options.fillStyle;
 }
 
